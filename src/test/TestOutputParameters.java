d68c03247ca2bb94dfef803f7431ff3d843a52e0
Terrence Cole <terrence@zettabytestorage.com>
2011-07-18 15:10:00 -0700

    use the more correct assertEquals here

--- src/test/TestOutputParameters.java
+++ src/test/TestOutputParameters.java
@@ -13,9 +13,9 @@ public class TestOutputParameters {
 	@Test
 	public void testGetFilename() {
 		op = new vash.OutputParameters("foo.png", 64, 64);
-		Assert.assertTrue(op.getFilename().equals("foo.png"));
+		Assert.assertEquals("foo.png", op.getFilename());
 		op = new vash.OutputParameters("-", 64, 64);
-		Assert.assertTrue(op.getFilename().equals("-"));
+		Assert.assertEquals("-", op.getFilename());
 	}
 
 	@Test(expected=IllegalArgumentException.class)
@@ -26,24 +26,24 @@ public class TestOutputParameters {
 	@Test
 	public void testGetImageType() {
 		op = new vash.OutputParameters("foo.png", 64, 64);
-		Assert.assertTrue(op.getImageType().equals("png"));
+		Assert.assertEquals("png", op.getImageType());
 		op = new vash.OutputParameters("foo.bmp", 64, 64);
-		Assert.assertTrue(op.getImageType().equals("bmp"));
+		Assert.assertEquals("bmp", op.getImageType());
 		op = new vash.OutputParameters("foo.jpg", 64, 64);
-		Assert.assertTrue(op.getImageType().equals("jpeg"));
+		Assert.assertEquals("jpeg", op.getImageType());
 		op = new vash.OutputParameters("-", 64, 64);
-		Assert.assertTrue(op.getImageType().equals("png"));
+		Assert.assertEquals("png", op.getImageType());
 	}
 
 	@Test
 	public void testGetWidth() {
 		op = new vash.OutputParameters("foo.png", 42, 64);
-		Assert.assertTrue(op.getWidth() == 42);
+		Assert.assertEquals(42, op.getWidth());
 	}
 
 	@Test
 	public void testGetHeight() {
 		op = new vash.OutputParameters("foo.png", 64, 42);
-		Assert.assertTrue(op.getHeight() == 42);
+		Assert.assertEquals(42, op.getHeight());
 	}
 }
